92976b02413d383014287c684a8d53763656dc47
AstrexXx <astrex17@abv.bg>
2020-05-18 15:52:40 +0300

    Almost all Animations are added
    TODO: Right/Left/BackOff/Shoot for Animation,
    HUD Information for UI,
    Zombieman Behaviour for AI,
    Damage Shader;

--- HomeWork_ClassicDoomDemo/Assets/Scripts/Firing.cs
+++ HomeWork_ClassicDoomDemo/Assets/Scripts/Firing.cs
@@ -2,19 +2,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.UIElements;
 
 public class Firing : MonoBehaviour
 {
+
+    public RawImage head;
+    public RawImage weapon;
+
+    private Animator headAnimator;
+    private Animator weaponAnimator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        headAnimator = head.GetComponent<Animator>();
+        weaponAnimator = weapon.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            headAnimator.SetTrigger("Fire");
+            weaponAnimator.SetTrigger("Fire");
+        }
     }
 }
